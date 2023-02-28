3911fc91544f9cea81434e06c426f11a1884756d
Mishator <voyteh89@mail.ru>
2023-02-28 12:43:25 +0300

    Выполнил Задача 2, результаты задания вывел в консоль.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1 () {
+        System.out.println("Задача 1");
         int age = 18;
         if (age >= 18) {
             System.out.println("Поздравляем вас с совершеннолетием.");
         }
         if (age < 18) {
             System.out.println("Возраст совершеннолетия ещё не наступил, нужно подождать.");
+        }
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        int temperatureStreet = -10;
+        if (temperatureStreet <= -5) {
+            System.out.println("Холодно!!! Нужно надеть шапку!");
+                   }
+        if (temperatureStreet >= 5) {
+            System.out.println("Тепло!!! Шапку одевать не надо!");
         }
     }
 }

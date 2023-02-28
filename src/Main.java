4e043bfd4ddc27f3d3ed339a46907e463869bb4e
Mishator <voyteh89@mail.ru>
2023-02-28 11:57:15 +0300

    Выполнил Задача 1, результат задания вывел в консоль.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+    public static void task1 () {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Поздравляем вас с совершеннолетием.");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, нужно подождать.");
+        }
     }
 }

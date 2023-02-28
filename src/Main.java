9d5b4980cfa9fc0349d5f9759d287d19454c3f9d
Mishator <voyteh89@mail.ru>
2023-02-28 22:23:11 +0300

    Выполнил Задача 3, решения задачи выводится в консоль.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -22,5 +23,15 @@ public class Main {
         if (temperatureStreet >= 5) {
             System.out.println("Тепло!!! Шапку одевать не надо!");
         }
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+    int speedLimit = 59;
+    if (speedLimit > 60) {
+        System.out.println("Ваша скорость больше 60 км в час, придётся заплатить штраф.");
+    }
+    if (speedLimit <= 60) {
+        System.out.println("Ваша скорость в пределах 60 км в час, можно ехать спокойно, штраф не придёт.");
+    }
     }
 }
